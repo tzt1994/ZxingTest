5f8a915350c2a12a82861f53892d1d0329b8f4a5
tangzhentao <tangzhentao1994@139.com>
2019-05-05 18:40:27 +0800

    Initial commit

---
+++ app/src/main/java/com/tangzhentao/zxingtest/MainActivity.java
@@ -0,0 +1,67 @@
+package com.tangzhentao.zxingtest;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.client.android.CaptureActivity;
+import com.google.zxing.client.android.Intents;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvResult = findViewById(R.id.tv_result);
+        findViewById(R.id.btn_scan).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 11);
+                }else {
+                    startCapture();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 11) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCapture();
+            }else {
+                Toast.makeText(this, "用户拒绝了权限使用，无法扫描", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void startCapture() {
+        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+        intent.setAction(Intents.Scan.ACTION);
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            Bundle bundle = data.getExtras();
+            tvResult.setText(bundle.getString(Intents.Scan.RESULT));
+        }
+    }
+}
